2fb90d5edc49ab0ef6be8e973d30d76e31acd0d9
Remap Development <mohammadrizkyk@gmail.com>
2018-11-12 15:57:31 +0700

    modul belajar

---
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/Flex/FlexModulBelajar.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\Flex;
+
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\Constant\Flex\ComponentButtonStyle;
+use LINE\LINEBot\Constant\Flex\ComponentFontSize;
+use LINE\LINEBot\Constant\Flex\ComponentFontWeight;
+use LINE\LINEBot\Constant\Flex\ComponentGravity;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectMode;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectRatio;
+use LINE\LINEBot\Constant\Flex\ComponentImageSize;
+use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\Constant\Flex\ComponentMargin;
+use LINE\LINEBot\Constant\Flex\ComponentSpacing;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ButtonComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ImageComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class FlexModulBelajar
+{
+    private static $items = [
+        '111' => [
+            'photo' => 'https://kotlinlang.org/assets/images/open-graph/kotlin_250x250.png',
+            'name' => 'Kotlin',
+            'chapter' => 'Bab 1',
+            'chapterName' => 'Pengenalan Variabel',
+            'stock' => true,
+        ],
+        '112' => [
+            'photo' => 'https://kotlinlang.org/assets/images/open-graph/kotlin_250x250.png',
+            'name' => 'Kotlin',
+            'chapter' => 'Bab 2',
+            'chapterName' => 'Pembuatan Fungsi',
+            'stock' => true,
+        ],
+    ];
+
+    /**
+     * Create sample shopping flex message
+     *
+     * @return \LINE\LINEBot\MessageBuilder\FlexMessageBuilder
+     */
+    public static function get()
+    {
+        return FlexMessageBuilder::builder()
+            ->setAltText('Shopping')
+            ->setContents(new CarouselContainerBuilder([
+                self::createItemBubble(111),
+                self::createItemBubble(112)
+            ]));
+    }
+
+    private static function createItemBubble($itemId)
+    {
+        $item = self::$items[$itemId];
+        return BubbleContainerBuilder::builder()
+            ->setHero(self::createItemHeroBlock($item))
+            ->setBody(self::createItemBodyBlock($item))
+            ->setFooter(self::createItemFooterBlock($item));
+    }
+
+    private static function createItemHeroBlock($item)
+    {
+        return ImageComponentBuilder::builder()
+            ->setUrl($item['photo'])
+            ->setSize(ComponentImageSize::FULL)
+            ->setAspectRatio(ComponentImageAspectRatio::R20TO13)
+            ->setAspectMode(ComponentImageAspectMode::COVER);
+    }
+
+    private static function createItemBodyBlock($item)
+    {
+        $components = [];
+        $components[] = TextComponentBuilder::builder()
+            ->setText($item['name'])
+            ->setWrap(true)
+            ->setWeight(ComponentFontWeight::BOLD)
+            ->setSize(ComponentFontSize::XL);
+
+        $components[] = BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::BASELINE)
+            ->setContents([
+                TextComponentBuilder::builder()
+                    ->setText($item['chapter'])
+                    ->setWrap(true)
+                    ->setWeight(ComponentFontWeight::BOLD)
+                    ->setSize(ComponentFontSize::XL)
+                    ->setFlex(0),
+                TextComponentBuilder::builder()
+                    ->setText($item['chapterName'])
+                    ->setWrap(true)
+                    ->setWeight(ComponentFontWeight::BOLD)
+                    ->setSize(ComponentFontSize::SM)
+                    ->setFlex(0)
+            ]);
+
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setContents($components);
+    }
+
+    private static function createItemFooterBlock($item)
+    {
+        $color = $item['stock'] ? null : '#aaaaaa';
+        $cartButton = ButtonComponentBuilder::builder()
+            ->setStyle(ComponentButtonStyle::PRIMARY)
+            ->setColor($color)
+            ->setAction(
+                new UriTemplateActionBuilder(
+                    'Mulai Belajar',
+                    'https://example.com'
+                )
+            );
+
+        $wishButton = ButtonComponentBuilder::builder()
+            ->setAction(
+                new UriTemplateActionBuilder(
+                    'Tambahkan ke Favorite',
+                    'https://example.com'
+                )
+            );
+
+        return BoxComponentBuilder::builder()
+            ->setLayout(ComponentLayout::VERTICAL)
+            ->setSpacing(ComponentSpacing::SM)
+            ->setContents([$cartButton, $wishButton]);
+    }
+
+    private static function createMoreBubble()
+    {
+        return BubbleContainerBuilder::builder()
+            ->setBody(
+                BoxComponentBuilder::builder()
+                    ->setLayout(ComponentLayout::VERTICAL)
+                    ->setSpacing(ComponentSpacing::SM)
+                    ->setContents([
+                        ButtonComponentBuilder::builder()
+                            ->setFlex(1)
+                            ->setGravity(ComponentGravity::CENTER)
+                            ->setAction(new UriTemplateActionBuilder('See more', 'https://example.com'))
+                    ])
+            );
+    }
+}
--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/TextMessageHandler.php
@@ -36,6 +36,7 @@ use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\KitchenSink\EventHandler;
 use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\Flex\FlexSampleRestaurant;
 use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\Flex\FlexSampleShopping;
+use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\Flex\FlexModulBelajar;
 use LINE\LINEBot\KitchenSink\EventHandler\MessageHandler\Util\UrlBuilder;
 use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
 use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
@@ -86,127 +87,11 @@ class TextMessageHandler implements EventHandler
         $replyToken = $this->textMessage->getReplyToken();
         $this->logger->info("Got text message from $replyToken: $text");
 
-        switch ($text) {
-            case 'profile':
-                $userId = $this->textMessage->getUserId();
-                $this->sendProfile($replyToken, $userId);
-                break;
-            case 'bye':
-                if ($this->textMessage->isRoomEvent()) {
-                    $this->bot->replyText($replyToken, 'Leaving room');
-                    $this->bot->leaveRoom($this->textMessage->getRoomId());
-                    break;
-                }
-                if ($this->textMessage->isGroupEvent()) {
-                    $this->bot->replyText($replyToken, 'Leaving group');
-                    $this->bot->leaveGroup($this->textMessage->getGroupId());
-                    break;
-                }
-                $this->bot->replyText($replyToken, 'Bot cannot leave from 1:1 chat');
-                break;
-            case 'confirm':
-                $this->bot->replyMessage(
-                    $replyToken,
-                    new TemplateMessageBuilder(
-                        'Confirm alt text',
-                        new ConfirmTemplateBuilder('Do it?', [
-                            new MessageTemplateActionBuilder('Yes', 'Yes!'),
-                            new MessageTemplateActionBuilder('No', 'No!'),
-                        ])
-                    )
-                );
-                break;
-            case 'buttons':
-                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
-                $buttonTemplateBuilder = new ButtonTemplateBuilder(
-                    'My button sample',
-                    'Hello my button',
-                    $imageUrl,
-                    [
-                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
-                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
-                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
-                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
-                    ]
-                );
-                $templateMessage = new TemplateMessageBuilder('Button alt text', $buttonTemplateBuilder);
-                $this->bot->replyMessage($replyToken, $templateMessage);
-                break;
-            case 'carousel':
-                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
-                $carouselTemplateBuilder = new CarouselTemplateBuilder([
-                    new CarouselColumnTemplateBuilder('foo', 'bar', $imageUrl, [
-                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
-                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
-                    ]),
-                    new CarouselColumnTemplateBuilder('buz', 'qux', $imageUrl, [
-                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
-                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
-                    ]),
-                ]);
-                $templateMessage = new TemplateMessageBuilder('Button alt text', $carouselTemplateBuilder);
-                $this->bot->replyMessage($replyToken, $templateMessage);
-                break;
-            case 'imagemap':
-                $richMessageUrl = UrlBuilder::buildUrl($this->req, ['static', 'rich']);
-                $imagemapMessageBuilder = new ImagemapMessageBuilder(
-                    $richMessageUrl,
-                    'This is alt text',
-                    new BaseSizeBuilder(1040, 1040),
-                    [
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/manga/en',
-                            new AreaBuilder(0, 0, 520, 520)
-                        ),
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/music/en',
-                            new AreaBuilder(520, 0, 520, 520)
-                        ),
-                        new ImagemapUriActionBuilder(
-                            'https://store.line.me/family/play/en',
-                            new AreaBuilder(0, 520, 520, 520)
-                        ),
-                        new ImagemapMessageActionBuilder(
-                            'URANAI!',
-                            new AreaBuilder(520, 520, 520, 520)
-                        )
-                    ]
-                );
-                $this->bot->replyMessage($replyToken, $imagemapMessageBuilder);
-                break;
-            case 'restaurant':
-                $flexMessageBuilder = FlexSampleRestaurant::get();
-                $this->bot->replyMessage($replyToken, $flexMessageBuilder);
-                break;
-            case 'shopping':
-                $flexMessageBuilder = FlexSampleShopping::get();
-                $this->bot->replyMessage($replyToken, $flexMessageBuilder);
-                break;
-            case 'quickReply':
-                $postback = new PostbackTemplateActionBuilder('Buy', 'action=quickBuy&itemid=222', 'Buy');
-                $datetimePicker = new DatetimePickerTemplateActionBuilder(
-                    'Select date',
-                    'storeId=12345',
-                    'datetime',
-                    '2017-12-25t00:00',
-                    '2018-01-24t23:59',
-                    '2017-12-25t00:00'
-                );
-
-                $quickReply = new QuickReplyMessageBuilder([
-                    new QuickReplyButtonBuilder(new LocationTemplateActionBuilder('Location')),
-                    new QuickReplyButtonBuilder(new CameraTemplateActionBuilder('Camera')),
-                    new QuickReplyButtonBuilder(new CameraRollTemplateActionBuilder('Camera roll')),
-                    new QuickReplyButtonBuilder($postback),
-                    new QuickReplyButtonBuilder($datetimePicker),
-                ]);
-
-                $messageTemplate = new TextMessageBuilder('Text with quickReply buttons', $quickReply);
-                $this->bot->replyMessage($replyToken, $messageTemplate);
-                break;
-            default:
-                $this->echoBack($replyToken, $text);
-                break;
+        if (strpos($text, 'kotlin') == true) {
+            $flexMessageBuilder = FlexModulBelajar::get();
+            $this->bot->replyMessage($replyToken, $flexMessageBuilder);
+        } else {
+            $this->echoBack($replyToken, "Mohon maaf kami masih belum bisa merespon chat anda, sabar nggih");
         }
     }
 
@@ -247,3 +132,124 @@ class TextMessageHandler implements EventHandler
         );
     }
 }
+
+// case 'profile':
+//                 $userId = $this->textMessage->getUserId();
+//                 $this->sendProfile($replyToken, $userId);
+//                 break;
+//             case 'bye':
+//                 if ($this->textMessage->isRoomEvent()) {
+//                     $this->bot->replyText($replyToken, 'Leaving room');
+//                     $this->bot->leaveRoom($this->textMessage->getRoomId());
+//                     break;
+//                 }
+//                 if ($this->textMessage->isGroupEvent()) {
+//                     $this->bot->replyText($replyToken, 'Leaving group');
+//                     $this->bot->leaveGroup($this->textMessage->getGroupId());
+//                     break;
+//                 }
+//                 $this->bot->replyText($replyToken, 'Bot cannot leave from 1:1 chat');
+//                 break;
+//             case 'confirm':
+//                 $this->bot->replyMessage(
+//                     $replyToken,
+//                     new TemplateMessageBuilder(
+//                         'Confirm alt text',
+//                         new ConfirmTemplateBuilder('Do it?', [
+//                             new MessageTemplateActionBuilder('Yes', 'Yes!'),
+//                             new MessageTemplateActionBuilder('No', 'No!'),
+//                         ])
+//                     )
+//                 );
+//                 break;
+//             case 'buttons':
+//                 $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
+//                 $buttonTemplateBuilder = new ButtonTemplateBuilder(
+//                     'My button sample',
+//                     'Hello my button',
+//                     $imageUrl,
+//                     [
+//                         new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
+//                         new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
+//                         new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
+//                         new MessageTemplateActionBuilder('Say message', 'hello hello'),
+//                     ]
+//                 );
+//                 $templateMessage = new TemplateMessageBuilder('Button alt text', $buttonTemplateBuilder);
+//                 $this->bot->replyMessage($replyToken, $templateMessage);
+//                 break;
+//             case 'carousel':
+//                 $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
+//                 $carouselTemplateBuilder = new CarouselTemplateBuilder([
+//                     new CarouselColumnTemplateBuilder('foo', 'bar', $imageUrl, [
+//                         new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
+//                         new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
+//                     ]),
+//                     new CarouselColumnTemplateBuilder('buz', 'qux', $imageUrl, [
+//                         new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
+//                         new MessageTemplateActionBuilder('Say message', 'hello hello'),
+//                     ]),
+//                 ]);
+//                 $templateMessage = new TemplateMessageBuilder('Button alt text', $carouselTemplateBuilder);
+//                 $this->bot->replyMessage($replyToken, $templateMessage);
+//                 break;
+//             case 'imagemap':
+//                 $richMessageUrl = UrlBuilder::buildUrl($this->req, ['static', 'rich']);
+//                 $imagemapMessageBuilder = new ImagemapMessageBuilder(
+//                     $richMessageUrl,
+//                     'This is alt text',
+//                     new BaseSizeBuilder(1040, 1040),
+//                     [
+//                         new ImagemapUriActionBuilder(
+//                             'https://store.line.me/family/manga/en',
+//                             new AreaBuilder(0, 0, 520, 520)
+//                         ),
+//                         new ImagemapUriActionBuilder(
+//                             'https://store.line.me/family/music/en',
+//                             new AreaBuilder(520, 0, 520, 520)
+//                         ),
+//                         new ImagemapUriActionBuilder(
+//                             'https://store.line.me/family/play/en',
+//                             new AreaBuilder(0, 520, 520, 520)
+//                         ),
+//                         new ImagemapMessageActionBuilder(
+//                             'URANAI!',
+//                             new AreaBuilder(520, 520, 520, 520)
+//                         )
+//                     ]
+//                 );
+//                 $this->bot->replyMessage($replyToken, $imagemapMessageBuilder);
+//                 break;
+//             case 'restaurant':
+//                 $flexMessageBuilder = FlexSampleRestaurant::get();
+//                 $this->bot->replyMessage($replyToken, $flexMessageBuilder);
+//                 break;
+//             case 'shopping':
+//                 $flexMessageBuilder = FlexSampleShopping::get();
+//                 $this->bot->replyMessage($replyToken, $flexMessageBuilder);
+//                 break;
+//             case 'quickReply':
+//                 $postback = new PostbackTemplateActionBuilder('Buy', 'action=quickBuy&itemid=222', 'Buy');
+//                 $datetimePicker = new DatetimePickerTemplateActionBuilder(
+//                     'Select date',
+//                     'storeId=12345',
+//                     'datetime',
+//                     '2017-12-25t00:00',
+//                     '2018-01-24t23:59',
+//                     '2017-12-25t00:00'
+//                 );
+
+//                 $quickReply = new QuickReplyMessageBuilder([
+//                     new QuickReplyButtonBuilder(new LocationTemplateActionBuilder('Location')),
+//                     new QuickReplyButtonBuilder(new CameraTemplateActionBuilder('Camera')),
+//                     new QuickReplyButtonBuilder(new CameraRollTemplateActionBuilder('Camera roll')),
+//                     new QuickReplyButtonBuilder($postback),
+//                     new QuickReplyButtonBuilder($datetimePicker),
+//                 ]);
+
+//                 $messageTemplate = new TextMessageBuilder('Text with quickReply buttons', $quickReply);
+//                 $this->bot->replyMessage($replyToken, $messageTemplate);
+//                 break;
+//             default:
+//                 $this->echoBack($replyToken, $text);
+//                 break;
